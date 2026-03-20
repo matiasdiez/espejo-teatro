0d4fe45e55bec9572a0c742aac0c2d10cac57f45
matiasdiez <diezmat@yahoo.com.ar>
2026-03-20 07:50:21 -0300

    Correción error en astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://matiasdiez.github.io',
-  // base: '/espejo-teatro', // Descomentar solo al hacer el build para GitHub Pages
+  site: 'https://matiasdiez.github.io',
+  base: '/espejo-teatro', // Descomentar solo al hacer el build para GitHub Pages
   vite: {
     plugins: [tailwindcss()],
   }
